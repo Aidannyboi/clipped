3bcd9adf7b11e2b61ad833a59efc05f77632a4fb
Aidan Castellanos <castellanosaidan@gmail.com>
2024-02-29 13:48:22 -0800

    Added model for Game options!

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,22 @@
+import {connect} from 'mongoose';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+async function dbConnect() {
+    try{
+        await connect (MONGODB_URI, {
+            dbName: 'ClippedDb'
+        });
+
+        console.log("Houston, we have connection to MongoDB")
+    } catch(error){
+        console.log(error)
+        throw error;
+    }
+}
+
+export default dbConnect;
---
+++ server/models/game.model.js
@@ -0,0 +1,35 @@
+import {model, Schema} from "mongoose"
+
+const GameSchema = new Schema (
+    {
+        GameName: {
+            type: String,
+
+            required : [true, "A game needs to have a name you know?"],
+
+            maxLength : [225, "Woah friend, that is way to many characters, lets stay under 225 characters."]
+        },
+
+        GameDescription: {
+            type: String,
+
+            required : [true, "Tell us about your game! it has to explained for others to understand it!"],
+
+            minLength: [10, "Lets get creative! Use at lest 10 or more characters please!"],
+
+            maxLength : [225, "That seems like a little to much. lets keep it under 225 characters."]
+        },
+
+        GameDevelopers: {
+            type: String,
+
+            required : [true, "The game has to have been made by someone! Tell us who made it!"],
+
+            maxLength : [225, "That seems like a little to much. lets keep it under 225 characters."]
+        }
+
+
+
+
+    }
+)
